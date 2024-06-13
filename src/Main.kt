0103c392d5920cb6088266a6531486fe7f964110
lolkarmalol <jhodier17q@gmail.com>
2024-06-13 09:10:18 -0500

    .
    Signed-off-by: lolkarmalol <jhodier17q@gmail.com>

--- src/Main.kt
+++ src/Main.kt
@@ -1,20 +1,76 @@
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main() {
-    println(birthdayGreeting("Rover", 5))
-    println(birthdayGreeting("Rex", 2))
-}
+    //5punto
+  /*
+   println(birthdayGreeting("Rover", 5))
 
+    println(birthdayGreeting("Rex", 2))
+    */
+     //2punto
+    //println("Happy Birthday, Rover!")
+    //println("You are now 5 years old!")
+     //birthdayGreeting()
+    //3punto
+    //birthdayGreeting()
+    //val greeting = birthdayGreeting()
+    //val greeting = birthdayGreeting()
+    //println(greeting)
+    //println(birthdayGreeting())
+    //4PUNTO
+
+    //println(birthdayGreeting("Rover"))
+    println(birthdayGreeting("Rover"))
+    println(birthdayGreeting("Rex"))
+
+
+
+}
 
 /*
 fun birthdayGreeting(name: String, age: Int): String {
     val nameGreeting = "Happy Birthday, $name!"
     val ageGreeting = "You are now 5 years old!"
     return "$nameGreeting\n$ageGreeting"
-}*/
+}
 
 fun birthdayGreeting(name: String, age: Int): String {
     val nameGreeting = "Happy Birthday, $name!"
     val ageGreeting = "You are now $age years old!"
     return "$nameGreeting\n$ageGreeting"
 }
+   */
+    //2punto
+    /*
+    fun birthdayGreeting() {
+    println("Happy Birthday, Rover!")
+    println("You are now 5 years old!")
+    }
+    */
+    //3punto
+    /*
+    fun birthdayGreeting(): Unit {
+        println("Happy Birthday, Rover!")
+        println("You are now 5 years old!")
+    }
+    */
+    /*fun birthdayGreeting(): String {
+    val nameGreeting = "Happy Birthday, Rover!"
+    val ageGreeting = "You are now 5 years old!"
+    return "$nameGreeting\n$ageGreeting"
+    }*/
+
+    //4PUNTO
+    /*fun birthdayGreeting(name: String): String {
+    val nameGreeting = "Happy Birthday, Rover!"
+    val ageGreeting = "You are now 5 years old!"
+    return "$nameGreeting\n$ageGreeting"
+}*/
+    fun birthdayGreeting(name: String): String {
+    val nameGreeting = "Happy Birthday, $name!"
+    val ageGreeting = "You are now 5 years old!"
+    return "$nameGreeting\n$ageGreeting"
+}
+
+
+
